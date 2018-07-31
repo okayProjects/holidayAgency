de888808a1ac8c8b3a2e7eb00581edc94438605a
Krzysztof Ługowski <k.lugowski@yahoo.com>
2018-07-31 20:37:51 +0200

    Add new sections

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,28 @@ $(function(){
   
 });
 
+
+$(window).scroll(function() {
+  $("nav").addClass('navbar-scroll');
+});
+
+$(window).scroll(function() {
+  $("footer-bottom").addClass('las');
+});
+
+
+$('.wierusz-inner').mouseenter(function() {
+	$('.wierusz-center').fadeIn(3000);
+});
+
+$('.wierusz-inner').mouseleave(function() {
+	$('.wierusz-center').fadeOut();
+});
+
+$('.tomaszewski-inner').mouseenter(function() {
+	$('.priest-carousel-container .tomaszewski-center').fadeIn(3000);
+});
+
+$('.tomaszewski-inner').mouseleave(function() {
+	$('.priest-carousel-container .tomaszewski-center').fadeOut();
+});
