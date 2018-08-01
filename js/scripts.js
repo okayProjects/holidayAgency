d29f2ecd27cb0bdea9e12fbf8e13673fe1e33468
Krzysztof Ługowski <k.lugowski@yahoo.com>
2018-08-01 21:41:49 +0200

    Add new pages

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,95 @@
 $(function(){
   console.log('DOM loaded');
-  
 });
-
 
 $(window).scroll(function() {
   $("nav").addClass('navbar-scroll');
 });
 
 $(window).scroll(function() {
-  $("footer-bottom").addClass('las');
+  $("footer-bottom").addClass('bottom-fixed');
 });
 
 
-$('.wierusz-inner').mouseenter(function() {
+$('.wierusz-inner').hover(function() {
 	$('.wierusz-center').fadeIn();
-});
-
-$('.wierusz-inner').mouseleave(function() {
+	}, function() {
 	$('.wierusz-center').fadeOut();
 });
 
-$('.tomaszewski-inner').mouseenter(function() {
-	$('.priest-carousel-container .tomaszewski-center').fadeIn();
+
+$('.tomaszewski-inner').hover(function() {
+	$('.tomaszewski-center').fadeIn();
+    }, function() {
+    	$('.tomaszewski-center').fadeOut();
 });
 
-$('.tomaszewski-inner').mouseleave(function() {
-	$('.priest-carousel-container .tomaszewski-center').fadeOut();
+$('.current-updates-one').hover(function() {
+	$(this).removeClass('hover-style');
+	$('.current-updates-two').css('display', 'none');
+	$('.current-updates-three').css('display', 'none');
+	}, function() {
+		$(this).addClass('hover-style');
+		$('.current-updates-two').fadeIn(1500);
+		$('.current-updates-three').fadeIn(2000);
+})
+
+$('.current-updates-two').hover(function() {
+	$(this).removeClass('hover-style');
+	$('.current-updates-one').css('display', 'none');
+	$('.current-updates-three').css('display', 'none');
+	}, function() {
+		$(this).addClass('hover-style');
+		$('.current-updates-one').fadeIn(1500);
+		$('.current-updates-three').fadeIn(2000);
+})
+
+$('.current-updates-three').hover(function() {
+	$(this).removeClass('hover-style');
+	$('.current-updates-one').css('display', 'none');
+	$('.current-updates-two').css('display', 'none');
+	}, function() {
+		$(this).addClass('hover-style');
+		$('.current-updates-one').fadeIn(1500);
+		$('.current-updates-two').fadeIn(2000);
+})
+
+//intencje//
+
+
+$('.nunery').mouseover(function() {
+	$('.table-image-container img:first-child').css('display', 'none');
+	$('.table-image-container img:nth-child(3)').css('display', 'none');
+	$('.table-image-container img:nth-child(2)').css('display', 'block');
+});
+
+$('.nunery').mouseleave(function() {
+	$('.table-image-container img:first-child').css('display', 'block');
+	$('.table-image-container img:nth-child(2)').css('display', 'none');
+	$('.table-image-container img:nth-child(3)').css('display', 'none');
+});
+
+$('.fara').mouseover(function() {
+	$('.table-image-container img:first-child').css('display', 'none');
+	$('.table-image-container img:nth-child(2)').css('display', 'none');
+	$('.table-image-container img:nth-child(3)').css('display', 'block');	
+});
+
+$('.fara').mouseleave(function() {
+	$('.table-image-container img:first-child').css('display', 'block');
+	$('.table-image-container img:nth-child(2)').css('display', 'none');
+	$('.table-image-container img:nth-child(3)').css('display', 'none');	
+});
+
+//sakramenty//
+
+$(window).scroll(function() {
+  $("nav").addClass('navbar-scroll');
+});
+
+$('.scrollspy-example').scroll(function() {
+  $(this).css('backgroundColor', '#274747');
+  $(this).mouseleave(function() {
+  $(this).css('backgroundColor', '#125558');
+  })
 });
