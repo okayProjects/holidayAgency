eef28d6ca84355198c796a0acdfeacf75739f34d
Krzysztof Ługowski <k.lugowski@yahoo.com>
2018-09-09 23:37:33 +0200

    Modify buttons

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,49 @@
 $(function () {
 	console.log('DOM loaded');
 });
+//intencje//
+
+
+$('.nunery').mouseover(function () {
+	$('.table-image-container img:first-child').css('display', 'none');
+	$('.table-image-container img:nth-child(3)').css('display', 'none');
+	$('.table-image-container img:nth-child(2)').css('display', 'block');
+});
+
+$('.nunery').mouseleave(function () {
+	$('.table-image-container img:first-child').css('display', 'block');
+	$('.table-image-container img:nth-child(2)').css('display', 'none');
+	$('.table-image-container img:nth-child(3)').css('display', 'none');
+});
+
+$('.fara').mouseover(function () {
+	$('.table-image-container img:first-child').css('display', 'none');
+	$('.table-image-container img:nth-child(2)').css('display', 'none');
+	$('.table-image-container img:nth-child(3)').css('display', 'block');
+});
+
+$('.fara').mouseleave(function () {
+	$('.table-image-container img:first-child').css('display', 'block');
+	$('.table-image-container img:nth-child(2)').css('display', 'none');
+	$('.table-image-container img:nth-child(3)').css('display', 'none');
+});
+
+//sakramenty//
 
 $(window).scroll(function () {
 	$("nav").addClass('navbar-scroll');
 });
+
+$('.scrollspy-example').scroll(function () {
+	$(this).css('backgroundColor', '#274747');
+	$(this).mouseleave(function () {
+		$(this).css('backgroundColor', '#125558');
+	})
+});
+
+// $(window).scroll(function () {
+// 	$("nav").addClass('navbar-scroll');
+// });
 
 $(window).scroll(function () {
 	if ($(document).scrollTop() > 600) {
@@ -14,54 +53,87 @@ $(window).scroll(function () {
 	}
 });
 
+const updateButtonOne = document.querySelector('.buttonOpenOne');
 
-$('.current-updates-one').hover(function () {
-	$(this).removeClass('hover-style');
-	$('.current-updates-two').css('display', 'none');
-	$('.current-updates-three').css('display', 'none');
-	$('.current-updates-one i').css('display', 'none');
-}, function () {
-	$(this).addClass('hover-style');
-	$('.current-updates-two').css('display', 'block');
-	$('.current-updates-three').css('display', 'block');
-	$(".current-updates-one i").css('display', 'block');
-})
+function displayCardOne() {
+	const buttonOne = document.querySelector('.buttonOpenOne');
+	const updateHeading = document.querySelector('.updates-board h3');
+	const cardOne = document.querySelector('.current-updates-one');
+	const cardTwo = document.querySelector('.current-updates-two');
+	const pin = document.querySelector('.hover-style i');
 
-$('.current-updates-two').hover(function () {
-	$(this).removeClass('hover-style');
-	$('.current-updates-one').css('display', 'none');
-	$('.current-updates-three').css('display', 'none');
-	$('.current-updates-two i').css('display', 'none');
-}, function () {
-	$(this).addClass('hover-style');
-	$('.current-updates-one').css('display', 'block');
-	$('.current-updates-three').css('display', 'block');
-	$('.current-updates-two i').css('display', 'block');
-})
+	cardOne.classList.toggle('hover-style');
+	cardTwo.classList.toggle('hide');
+	updateHeading.classList.toggle('hide');
+	buttonOne.classList.toggle('hide');
+	pin.classList.add('hide');
+	// updateButtonOne.classList.toggle('afterClickButton');
+}
+updateButtonOne.addEventListener('click', displayCardOne);
 
-$('.current-updates-three').hover(function () {
-	$(this).removeClass('hover-style');
-	$('.current-updates-one').css('display', 'none');
-	$('.current-updates-two').css('display', 'none');
-	$('.current-updates-three i').css('display', 'none');
-}, function () {
-	$(this).addClass('hover-style');
-	$('.current-updates-one').css('display', 'block');
-	$('.current-updates-two').css('display', 'block');
-	$('.current-updates-three i').css('display', 'block');
-})
+
+const closeUpdateButtonOne = document.querySelector('.hover-style button:nth-of-type(2)');
+
+function showCardOne() {
+	const buttonOne = document.querySelector('.buttonOpenOne');
+	const updateHeading = document.querySelector('.updates-board h3');
+	const cardOne = document.querySelector('.current-updates-one');
+	const cardTwo = document.querySelector('.current-updates-two');
+	const pin = document.querySelector('.current-updates-one i');
+	cardOne.classList.toggle('hover-style');
+	cardTwo.classList.toggle('hide');
+	updateHeading.classList.toggle('hide');
+	buttonOne.classList.toggle('hide');
+	pin.classList.remove('hide');
+}
+closeUpdateButtonOne.addEventListener('click', showCardOne);
+
+
+const updateButtonTwo = document.querySelector('.buttonOpenTwo');
+
+function displayCardTwo() {
+	const buttonTwo = document.querySelector('.buttonOpenTwo');
+	const updateHeading = document.querySelector('.updates-board h3');
+	const cardOne = document.querySelector('.current-updates-one');
+	const cardTwo = document.querySelector('.current-updates-two');
+	const pin = document.querySelector('.current-updates-two i');
+	buttonTwo.classList.toggle('hide');
+	cardOne.classList.toggle('hide');
+	cardTwo.classList.toggle('hover-style');
+	updateHeading.classList.toggle('hide');
+	pin.classList.toggle('hide');
+}
+updateButtonTwo.addEventListener('click', displayCardTwo);
+
+
+const closeUpdateButtonTwo = document.querySelector('.buttonCloseTwo');
+
+function showCardTwo() {
+	const buttonTwo = document.querySelector('.buttonOpenTwo');
+	const updateHeading = document.querySelector('.updates-board h3');
+	const cardOne = document.querySelector('.current-updates-one');
+	const cardTwo = document.querySelector('.current-updates-two');
+	const pin = document.querySelector('.current-updates-two i');
+	buttonTwo.classList.toggle('hide');
+	cardOne.classList.toggle('hide');
+	cardTwo.classList.toggle('hover-style');
+	updateHeading.classList.toggle('hide');
+	pin.classList.toggle('hide');
+}
+closeUpdateButtonTwo.addEventListener('click', showCardTwo)
+
 
 $('.wierusz-inner').hover(function () {
-	$('.wierusz-center').fadeIn(3000);
+	$('.wierusz-center').fadeIn(1500);
 }, function () {
-	$('.wierusz-center').fadeOut(1000);
+	$('.wierusz-center').fadeOut(500);
 });
 
 
 $('.tomaszewski-inner').hover(function () {
-	$('.tomaszewski-center').fadeIn(3000);
+	$('.tomaszewski-center').fadeIn(1500);
 }, function () {
-	$('.tomaszewski-center').fadeOut(1000);
+	$('.tomaszewski-center').fadeOut(500);
 });
 
 
@@ -115,42 +187,3 @@ $(function() {
     });
 });
 */
-//intencje//
-
-
-$('.nunery').mouseover(function () {
-	$('.table-image-container img:first-child').css('display', 'none');
-	$('.table-image-container img:nth-child(3)').css('display', 'none');
-	$('.table-image-container img:nth-child(2)').css('display', 'block');
-});
-
-$('.nunery').mouseleave(function () {
-	$('.table-image-container img:first-child').css('display', 'block');
-	$('.table-image-container img:nth-child(2)').css('display', 'none');
-	$('.table-image-container img:nth-child(3)').css('display', 'none');
-});
-
-$('.fara').mouseover(function () {
-	$('.table-image-container img:first-child').css('display', 'none');
-	$('.table-image-container img:nth-child(2)').css('display', 'none');
-	$('.table-image-container img:nth-child(3)').css('display', 'block');
-});
-
-$('.fara').mouseleave(function () {
-	$('.table-image-container img:first-child').css('display', 'block');
-	$('.table-image-container img:nth-child(2)').css('display', 'none');
-	$('.table-image-container img:nth-child(3)').css('display', 'none');
-});
-
-//sakramenty//
-
-$(window).scroll(function () {
-	$("nav").addClass('navbar-scroll');
-});
-
-$('.scrollspy-example').scroll(function () {
-	$(this).css('backgroundColor', '#274747');
-	$(this).mouseleave(function () {
-		$(this).css('backgroundColor', '#125558');
-	})
-});
